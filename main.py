8c3e6219ca284582c5062a065c19ccd76e767dcc
jumar <jumarjosebravinpaulino@gmail.com>
2025-08-01 19:25:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,9 @@
 from datetime import datetime, timedelta
 
-# Pede para o usuário digitar a data
 data_input = input("Digite a data do ocorrido no formato dd/mm/aaaa: ")
-
-# Converte a string digitada para um objeto datetime
 data_inicial = datetime.strptime(data_input, "%d/%m/%Y")
-
-# Pede a quantidade de dias a somar
 dias = 20
-
-# Cria o intervalo e soma à data
 nova_data = data_inicial + timedelta(days=dias)
-
-# Mostra o resultado
-
 velo = float(input('qual a velocidade do carro:'))
 if velo <= 80:
     print('tudo ok')
@@ -21,5 +11,6 @@ elif velo > 80:
     print("vece esta multado")
     multa = (velo-80)*7
 print('você esta a mais de oitenta km por hora sua multa e de {} e voce possui ate o dia {} para efetuar o pagamento'.format(multa, nova_data))
+
 
 
